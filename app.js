88864e85d79832b73a861d908bd0a94138acaa2b
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-16 18:17:16 -0400

    setup schema and model

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI).then(()=>console.log('Connected to DB...'));
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (err) {
     console.log(err);
