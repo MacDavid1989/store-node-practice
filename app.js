43bfd9925a0b9aaa7f583a53182264132995e4e7
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-16 17:55:40 -0400

    connect to DB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
 
 // async errors
 
@@ -23,6 +24,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (err) {
     console.log(err);
